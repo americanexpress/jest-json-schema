44f260d1e70aee26817a81cfd6ca49326e0651db
Jimmy King <james.king@aexp.com>
2017-06-01 08:35:08 -0700

    feat(matchers): add new toBeValidSchema matcher

---
+++ __tests__/matchers/toBeValidSchema.spec.js
@@ -0,0 +1,52 @@
+const toBeValidSchemaUnderTest = require('../../matchers/toBeValidSchema');
+
+expect.extend({
+  toBeValidSchemaUnderTest,
+});
+
+describe('toBeValidSchema', () => {
+  let schema;
+
+  beforeEach(() => {
+    schema = {
+      properties: {
+        hello: { type: 'string' },
+      },
+      required: ['hello'],
+    };
+  });
+
+  it('does not throw', () => {
+    expect(schema).toBeValidSchemaUnderTest();
+  });
+
+  it('fails schemas with invalid properties', () => {
+    schema.properties = [];
+    expect(() => expect(schema).toBeValidSchemaUnderTest())
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('fails schemas of invalid types', () => {
+    expect(() => expect('schema').toBeValidSchemaUnderTest())
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('fails for valid schema when using .not', () => {
+    expect(() => expect(schema).not.toBeValidSchemaUnderTest())
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('assertion error matcherResult property contains matcher name and actual value', () => {
+    schema.properties = [];
+    try {
+      expect(schema).toBeValidSchemaUnderTest();
+    } catch (error) {
+      expect(error.matcherResult).toEqual({
+        actual: schema,
+        message: expect.any(Function),
+        name: 'toBeValidSchema',
+        pass: false,
+      });
+    }
+  });
+});
---
+++ matchers/toBeValidSchema.js
@@ -0,0 +1,36 @@
+const Ajv = require('ajv');
+const chalk = require('chalk');
+const { matcherHint } = require('jest-matcher-utils');
+
+function toBeValidSchema(received) {
+  const ajv = new Ajv({
+    allErrors: true,
+  });
+
+  const pass = ajv.validateSchema(received);
+
+  const message = pass
+    ? () => `${matcherHint('.not.toBeValidSchema', 'received', '')}\n\nExpected input not to be a valid JSON schema`
+    : () => {
+      let messageToPrint = 'schema\n';
+      ajv.errors.forEach((error) => {
+        let line = error.message;
+
+        if (error.dataPath) {
+          line = `${error.dataPath} ${error.message}`;
+        }
+
+        messageToPrint += chalk.red(`  ${line}\n`);
+      });
+      return `${matcherHint('.toBeValidSchema', 'schema', '')}\n\n${messageToPrint}`;
+    };
+
+  return {
+    actual: received,
+    message,
+    name: 'toBeValidSchema',
+    pass,
+  };
+}
+
+module.exports = toBeValidSchema;
